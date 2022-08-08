7a853ff25fb43bd847a3a608a679b58754d32322
mye8503 <106449396+mye8503@users.noreply.github.com>
2022-08-08 10:25:42 -0400

    updated baseline model

--- baseline_model.py
+++ baseline_model.py
@@ -3,61 +3,85 @@ import csv
 results = []
 
 # collect true outputs
-with open('output.csv', newline='') as csvfile:
+with open('PLATINUM_IV_output.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile)
 
     for row in spamreader:
-        if len(row) == 0: results.append(['0'])
-        elif row[0] == 'result': continue
-        else: results.append(row)
+        results.append([row[0]])
 
 correct = 0
 total = 0
 
-with open('input.csv', newline='') as csvfile:
+
+with open('PLATINUM_IV_input.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile)
+
+    rival_win_idx = [0] * 5
+    rival_loss_idx = [0] * 5
+    rival_rates = [0] * 5
+
+    ally_win_idx = [0] * 5
+    ally_loss_idx = [0] * 5
+    ally_rates = [0] * 5
 
     i = 0
     for row in spamreader:
-        if row[0] == "main_player_win":
-
+        if i == 200: print(row)
+        if i == 0:
             # get indices of win & loss counts for each player
+            for j in range(len(row)):
+                column = row[j].split("_")
 
-            print("skipped first row")
-            continue
+                if(column[0] == "rival"):
+                    if(column[2] == "wins"):
+                        rival_win_idx[int(column[1])] = j
+                    
+                    elif(column[2] == "losses"):
+                        rival_loss_idx[int(column[1])] = j
 
-        allyrate = 0
-        rivalrate = 0
+                elif(column[0] == "ally"):
+                    if(column[2] == "wins"):
+                        ally_win_idx[int(column[1])] = j
+                    
+                    elif(column[2] == "losses"):
+                        ally_loss_idx[int(column[1])] = j
 
-        # get win rate for all allies & rivals
-        a1rate = int(row[0]) / (int(row[0]) + int(row[2]))
-        a2rate = int(row[9]) / (int(row[9]) + int(row[11]))
-        a3rate = int(row[18]) / (int(row[18]) + int(row[20]))
-        a4rate = int(row[27]) / (int(row[27]) + int(row[29]))
-        a5rate = int(row[36]) / (int(row[36]) + int(row[36]))
+                elif(column[0] == "main"):
+                    if(column[2] == "win"):
+                        ally_win_idx[4] = j
+                    
+                    elif(column[2] == "losses"):
+                        ally_loss_idx[4] = j
 
-        r1rate = int(row[45]) / (int(row[45]) + int(row[47]))
-        r2rate = int(row[54]) / (int(row[54]) + int(row[56]))
-        r3rate = int(row[63]) / (int(row[63]) + int(row[63]))
-        r4rate = int(row[72]) / (int(row[72]) + int(row[74]))
-        r5rate = int(row[81]) / (int(row[81]) + int(row[83]))
+        else:
+            # get win rate for all allies & rivals
+            for j in range(len(ally_win_idx)):
+                # rate = win / (win + loss)
+                ally_rates[j] = int(row[ally_win_idx[j]]) / (int(row[ally_win_idx[j]]) + int(row[ally_loss_idx[j]]))
 
-        allyrate = (a1rate + a2rate + a3rate + a4rate + a5rate) / 5
-        rivalrate = (r1rate + r2rate + r3rate + r4rate + r5rate) / 5
+            for j in range(len(rival_win_idx)):
+                rival_rates[j] = int(row[rival_win_idx[j]]) / (int(row[rival_win_idx[j]]) + int(row[rival_loss_idx[j]]))
+                
+            ally_rate = sum(ally_rates) / 5
+            rival_rate = sum(rival_rates) / 5
 
-        if(allyrate > rivalrate): results[i].append('1')
-        else: results[i].append('0')
+            if(ally_rate > rival_rate): results[i].append('1')
+            else: results[i].append('0')
 
-        if results[i][0] == results[i][1]: correct += 1
-        total += 1
+            # check against actual output
+            if results[i][0] == results[i][1]: correct += 1
+            total += 1
 
         i += 1
 
 print("CORRECT:", correct, "out of", total)
 
-with open('test_output.csv', 'w', newline='') as csvfile:
-    spamwriter = csv.writer(csvfile)
+print(results)
 
-    for row in results:
-        spamwriter.writerow(row)
+# display results
+# with open('test_output.csv', 'w', newline='') as csvfile:
+#     spamwriter = csv.writer(csvfile)
+
+#     for row in results:
+#         spamwriter.writerow(row)
 
